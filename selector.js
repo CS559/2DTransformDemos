c82b5ed8402d83623af2e88b61a1e3812a2d2b15
Michael Gleicher <gleicher@cs.wisc.edu>
2021-02-03 20:44:08 -0600

    switch to load json

--- selector.js
+++ selector.js
@@ -1,3 +1,6 @@
+/* jshint -W069, esversion:6 */
+// @ts-check
+export {};
 import { createExample, makeSelect } from "./transformToy.js";
 
 /**
@@ -15,145 +18,42 @@ headingDiv.appendChild(headerTitle);
 let br = document.createElement("br");
 document.getElementsByTagName("body")[0].appendChild(br);
 
-let examples = [
-    {
-        title: "Scale about a 45 degrees Axis",
-        transformations:
-            [
-                ["rotate", 45],
-                ["scale", 2, 1],
-                ["rotate", -45],
-                ["fillRect", -10, -10, 20, 20, "#F0000080"]
-            ]
-    }, {
-        title: "Scale about a 45 degrees Axis (w/original square)",
-        transformations:
-            [
-                ["fillRect", -10, -10, 20, 20, "#800000"],
-                ["rotate", 45],
-                ["scale", 2, 1],
-                ["rotate", -45],
-                ["fillRect", -10, -10, 20, 20, "#F0000080"]
-            ]
-    }, {
-        title: "Triangle Test",
-        transformations:
-            [
-                ["triangle", 0, 0, "red"],
-                ["translate", 20, 0],
-                ["triangle", 0, 0, "blue"],
-                ["triangle", 10, 10, "blue"]
-            ]
-    }, {
-        title: "NU Scale and then rotate",
-        transformations:
-            [
-                ["scale", 2, 1],
-                ["rotate", 45],
-                ["fillRect", -10, -10, 20, 20]
-            ]
-    }, {
-        title: "Rotate then scale NU",
-        transformations:
-            [
-                ["rotate", 45],
-                ["scale", 2, 1],
-                ["fillRect", -10, -10, 20, 20]
-            ]
-    }, {
-        title: "Bend an arm 45 degrees at Elbow and Wrist",
-        transformations:
-            [
-                ["fillRect", 0, 0, 20, 10, "purple"],
-                ["translate", 20, 0],
-                ["rotate", 45],
-                ["fillRect", 0, 0, 20, 10, "blue"],
-                ["translate", 20, 0],
-                ["rotate", 45],
-                ["fillRect", 0, 0, 10, 10, "green"]
-            ]
-    }, {
-        title: "Rotate about object center",
-        transformations:
-            [
-                ["translate", 30, 30],
-                ["rotate", 45],
-                ["translate", -30, -30],
-                ["fillRect", 20, 20, 40, 40]
-            ]
-    }, {
-        title: "Save and restore",
-        transformations:
-            [
-                ["save"],
-                ["translate", 30, 30],
-                ["rotate", 45],
-                ["fillRect", 0, 0, 30, 30, "red"],
-                ["restore"],
-                ["translate", -30, -30],
-                ["rotate", -45],
-                ["fillRect", 0, 0, 30, 30]
-            ]
-    }, {
-        title: "More save and restore",
-        transformations:
-            [
-                ["translate", 20, 20],
-                ["fillRect", 0, 0, 20, 20, "yellow"],
-                ["save"],
-                ["translate", -20, -20],
-                ["rotate", 45],
-                ["fillRect", 0, 0, 20, 20, "red"],
-                ["save"],
-                ["rotate", -45],
-                ["translate", -40, 20],
-                ["fillRect", 0, 0, 20, 20],
-                ["restore"],
-                ["scale", 1.5, 1.5],
-                ["fillRect", -20, -20, 10, 10, "purple"],
-                ["restore"],
-                ["fillRect", -30, -40, 20, 20, "green"]
-            ]
-    }, {
-        title: "Test bad saves and restores",
-        transformations:
-            [
-                ["restore"],
-                ["restore"],
-                ["restore"],
-                ["save"],
-                ["save"],
-                ["save"],
-                ["fillRect", 0, 0, 20, 20, "green"]
-            ]
-    }, {
-        title: "Your turn to play with it",
-    }
-];
+// get the filename for the examples list as the query string
+// don't forget to remove the "?"
+const filename = window.location.search.substring(1) || "examples.json";
+console.log(`getting example list file ${filename}`) ;
 
-let titles = ["Please select one example"];
-let exampleDivs = [];
-examples.forEach(e => {
-    exampleDivs.push(createExample(e.title, e.transformations));
-    titles.push(e.title);
-});
+fetch(filename).then(response => response.json())
+    .then(data => selectAndGo(data))
+    .catch(error => console.log(`Error loading ${filename} - ${error}`));
 
-// make a dropdown menu to select examples
-let selectExample = makeSelect(titles, headingDiv);
-selectExample.id = "exampleSelect";
+function selectAndGo(examples) {
+    console.log(examples);
 
-// switch between different examples
-let currentExample;
-selectExample.onchange = function () {
-    document.getElementById("headingDiv").style.paddingTop = 0;
-    if (currentExample) currentExample.style.display = "none";
-    let selectedTitle = selectExample.options[selectExample.selectedIndex].text;
-    exampleDivs.forEach(ed => {
-        if (ed.id == selectedTitle + "-example") {
-            currentExample = ed;
-        }
+    let titles = ["Please select one example"];
+    let exampleDivs = [];
+    examples.forEach(e => {
+        exampleDivs.push(createExample(e.title, e.transformations));
+        titles.push(e.title);
     });
-    if (currentExample){ 
-        currentExample.style.display = "flex";
-    }
-};
+
+    // make a dropdown menu to select examples
+    let selectExample = makeSelect(titles, headingDiv);
+    selectExample.id = "exampleSelect";
+
+    // switch between different examples
+    let currentExample;
+    selectExample.onchange = function () {
+        document.getElementById("headingDiv").style.paddingTop = 0;
+        if (currentExample) currentExample.style.display = "none";
+        let selectedTitle = selectExample.options[selectExample.selectedIndex].text;
+        exampleDivs.forEach(ed => {
+            if (ed.id == selectedTitle + "-example") {
+                currentExample = ed;
+            }
+        });
+        if (currentExample){ 
+            currentExample.style.display = "flex";
+        }
+    };
+}
